3e646d889d88f41efcc841de5eb04c78bbc231bb
Belizare <bellemel64@gmail.com>
2022-03-22 14:02:35 -0400

    updated js

--- src/index.js
+++ src/index.js
@@ -71,6 +71,7 @@ function displayWeatherCondition(response) {
     cityName.innerHTML = response.data.name;
     let countryName = document.querySelector("#country-name");
     countryName.innerHTML = response.data.sys.country;
+    getForecast(response.data.coord);
 }
 
 function searchCity(city) {
@@ -115,6 +116,43 @@ function convertToCelsius(event) {
   let temperature = celsiusTemperature;
   temperatureElement.innerHTML = Math.round(temperature);
   document.querySelector("#celc").innerHTML = `°C`;
+}
+function displayForecast(response) {
+  console.log(response.data)
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 let celsiusTemperature = null;
